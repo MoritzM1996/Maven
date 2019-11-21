5a9faeb022216587909d01451e9ea3e6f4b435ee
MoritzM1996 <33116431+MoritzM1996@users.noreply.github.com>
2019-11-21 11:54:03 +0100

    Maven Upload
    Maven Upload

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,43 @@
+package de.hfu;
+import java.util.*;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		String test = new String();
+		System.out.print("Bitte geben Sie einen Text ein: ");
+		test = scan.nextLine();
+		int j = 0;
+		while(j < test.length())
+		{
+            if(test.charAt(j) > 96 && test.charAt(j) < 123)
+			{
+            	System.out.print((char)(test.charAt(j) - 32));
+				j++;
+			}
+            else
+			{
+				System.out.print(test.charAt(j));
+				j++;
+			}
+		}
+		System.out.println();
+	}
+}
+
+
+/**
+ * Hello world!
+ *
+ */
+ /*
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+*/
